fb1fc0c070aea9cfd76bb5c1e542cfb78678a13e
josedanCanaima <josedanq100@gmail.com>
2017-12-13 16:15:34 -0400

    Configurado el debug server de php, y configurado el webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,48 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const config = {
+  entry: './js/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js'
+  },
+
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [
+      {
+         test: /\.js$/,
+         loader: 'babel-loader',
+         exclude: /node_modules/,
+         options: {
+          presets: [
+            'babel-preset-es2015',
+            'babel-preset-env'
+          ].map(require.resolve)
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new UglifyJsPlugin() //Minificar el bundle
+  ]
+}
+
+
+module.exports = config;
