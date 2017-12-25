230de763170deef4635445fab94f51d5dc7ae452
josedanCanaima <josedanq100@gmail.com>
2017-12-25 14:36:13 -0400

    Se agregaron los links de las redes sociales

--- includes/footer.php
+++ includes/footer.php
@@ -5,12 +5,10 @@
 		<div class="social">
 			<p>¡SÍGUENOS!<br /></p>
 			<div class="icons">
-				<a href=""><span class="icon icon-facebook-with-circle"></span></a>
-				<a href=""><span class="icon icon-instagram-with-circle"></span></a>
-				<a href=""><span class="icon icon-twitter-with-circle"></span></a>
+				<a href="https://www.facebook.com/GUIAUSB18" target="_blank"><span class="icon icon-facebook-with-circle"></span></a>
+				<a href="https://www.instagram.com/GUIA_USB" target="_blank"><span class="icon icon-instagram-with-circle"></span></a>
+				<a href="https://www.twitter.com/GUIA_USB" target="_blank"><span class="icon icon-twitter-with-circle"></span></a>
 			</div>
 		</div>
 		<div class="developer">Contacta al desarrollador: <br/><br />josedanq100@gmail.com</div>
 	</footer>
-
-	
