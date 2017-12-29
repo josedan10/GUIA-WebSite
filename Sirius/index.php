60f053ad29ac7e0586bdcc2f5d0620a9f2504aed
josedanCanaima <josedanq100@gmail.com>
2017-12-29 19:37:39 -0400

    Implementado el Script y los estilos para generar el inicio del blog

--- Sirius/index.php
+++ Sirius/index.php
@@ -1,3 +1,42 @@
+<?php 
+	//Conexión a BD
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "Zedstaphplis07.";
+	$dbname = "guiabd";
+
+	// Creamos la conexión
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Verificamos la conexión
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+	$Query = "SELECT COUNT(Titulo) AS TOTAL FROM articulo;";
+	$resultado = $conn->query($Query);
+
+	$row = $resultado->fetch_assoc();
+
+	//$TOTAL = $row["TOTAL"];
+	$TOTAL = 91;
+	$maxPags = 10;
+
+	// switch($TOTAL%10){
+	// 	case 0:
+	// 		echo "El número total de páginas es: ".($TOTAL/10)."\n";
+	// 		echo "En la última página hay 10 artículos";
+	// 		break;
+
+	// 	default:
+	// 		echo "El número total de páginas es: ".(floor($TOTAL/10) + 1)."\n";
+	// 		echo "En la última página hay ".($TOTAL%10)." artículos";
+	// 		break;
+	// }	
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,8 +66,125 @@
 			<h1 class="title-blog">SIRIUS</h1>
 			
 			<!-- Articulos -->
+			<?php 
 
-			<article>
+				//Hacemos la consulta de manera tal que los artículos vengan ordenados desde los más recientes a los más antiguos
+					
+				$queryArt = "SELECT * FROM articulo ORDER BY Fecha DESC;";
+				$resultadoArticulos = $conn->query($queryArt);
+
+
+				if($TOTAL <= 10){
+					//Una sola página, no hace falta indice de navegación
+					
+					while($articulo = $resultadoArticulos->fetch_assoc()) {
+						
+				        echo "<article>
+							<h2>".$articulo["Titulo"]."</h2>
+							<div class='atributos-articulo'>
+								<span class='icon icon-calendar'></span>".$articulo["Fecha"]."
+								<span class='icon icon-price-tag'></span>Sin categoria
+							</div>
+							
+							<img src='articulo1/imgArticule1.jpeg' alt='' />
+							<div class='articulo'>".$articulo["Contenido"]."</div>
+							
+						</article>";
+				    }
+
+				}else{
+				
+					for($i = 0; $i < 10; $i++) {
+						$articulo = $resultadoArticulos->fetch_assoc();
+				        echo "<article>
+							<h2>".$articulo["Titulo"]."</h2>
+							<div class='atributos-articulo'>
+								<span class='icon icon-calendar'></span>".$articulo["Fecha"]."
+								<span class='icon icon-price-tag'></span>Sin categoria
+							</div>
+							
+							<img src='articulo1/imgArticule1.jpeg' alt='' />
+							<div class='articulo'>".$articulo["Contenido"]."</div>
+							
+						</article>";
+				    }
+				}
+
+				$spans = "";
+
+				switch($TOTAL%10){
+					case 0:
+						// echo "El número total de páginas es: ".($TOTAL/10)."\n";
+						// echo "En la última página hay 10 artículos";
+
+						if(floor($TOTAL/10) > $maxPags){
+
+							$TOTAL = $maxPags;
+
+							for($i = 1; $i <= (floor($TOTAL/10)); $i++){
+						    	$spans = $spans."<span>".$i."</span>";
+						    }
+
+						    echo "
+						    <div class='navigation-bar'>
+						    	<span class='icon icon-chevron-thin-left'></span>".$spans."<span class='icon icon-chevron-thin-right'></span>
+						    </div>"
+						    ;
+
+						}else{
+
+							for($i = 1; $i <= floor($TOTAL/10) ; $i++){
+						    	$spans = $spans."<span>".$i."</span>";
+						    }
+
+						    echo "
+						    <div class='navigation-bar'>
+						    	<span class='icon icon-chevron-thin-left'></span>".$spans."<span class='icon icon-chevron-thin-right'></span>
+						    </div>"
+						    ;
+
+						}
+
+						break;
+
+					default:
+						// echo "El número total de páginas es: ".(floor($TOTAL/10) + 1)."\n";
+						// echo "En la última página hay ".($TOTAL%10)." artículos";
+						
+						if(floor($TOTAL/10) + 1 > $maxPags){
+							$TOTAL = $maxPags;
+
+							for($i = 1; $i <= (floor($TOTAL/10)); $i++){
+						    	$spans = $spans."<span>".$i."</span>";
+						    }
+
+						    echo "
+						    <div class='navigation-bar'>
+						    	<span class='icon icon-chevron-thin-left'></span>".$spans."<span class='icon icon-chevron-thin-right'></span>
+						    </div>"
+						    ;
+						}else{
+
+							for($i = 1; $i <= (floor($TOTAL/10) + 1); $i++){
+						    	$spans = $spans."<span>".$i."</span>";
+						    }
+
+						    echo "
+						    <div class='navigation-bar'>
+						    	<span class='icon icon-chevron-thin-left'></span>".$spans."<span class='icon icon-chevron-thin-right'></span>
+						    </div>"
+						    ;
+
+						}
+
+						break;
+				}	
+
+
+			    
+
+			?>
+			<!-- <article>
 				<h2>Titulo del artículo</h2>
 				<div class="atributos-articulo">
 					<span class="icon icon-calendar"></span>31 de agosto de 2017
@@ -53,7 +209,7 @@
 
 				<a href="articulo1"><button type="button" class="btn">LEER MÁS</button></a>
 			</article>
-			
+
 			<article>
 				<h2>Titulo del artículo</h2>
 				<div class="atributos-articulo">
@@ -65,7 +221,7 @@
 				<div class="articulo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil est, voluptates, totam quisquam quibusdam voluptas, odit ullam aliquam dolor repudiandae, neque obcaecati. Voluptate dolorum eius facere ipsam iusto, error non!</div>
 
 				<a href="articulo1"><button type="button" class="btn">LEER MÁS</button></a>
-			</article>
+			</article> -->
 			
 			<!-- Busqueda -->
 
