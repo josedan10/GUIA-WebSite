a1adceb90859dc88e6be3dafe305d296ceb5fffb
josedanCanaima <josedanq100@gmail.com>
2017-12-29 20:23:28 -0400

    Se elimina el código que está en conexionDB.php

--- Calendario/index.php
+++ Calendario/index.php
@@ -24,73 +24,7 @@
 
 		<div class="container">
 
-			<?php
-
-				//Conexión a BD
-
-				$servername = "localhost";
-				$username = "root";
-				$password = "Zedstaphplis07.";
-				$dbname = "guiabd";
-
-				// Creamos la conexión
-				$conn = new mysqli($servername, $username, $password, $dbname);
-
-				// Verificamos la conexión
-				if ($conn->connect_error) {
-				    die("Connection failed: " . $conn->connect_error);
-				}
-
-
-				/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-				$Meses = array(1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril', 5 => 'Mayo', 6 => 'Junio',
-					7 => 'Julio', 8 => 'Agosto', 9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre');
-
-				$diasSemana = array("Sunday" => 1, "Monday" => 2, "Tuesday" => 3, "Wednesday" => 4, 
-					"Thursday" => 5, "Friday" => 6, "Saturday" => 7);
-
-				
-				$fechaCompleta = date('Y/d/m');
-				$diaSemanaActual = $diasSemana[date('l')];		//Dia de la semana correspondiente al array $diasSemana
-
-				$arrayFecha = explode("/", $fechaCompleta); 	//Array de la fecha
-				$mesActual = $Meses[$arrayFecha[2]];			//Mes actual
-				$diaActual = $arrayFecha[1]; 					//Dia actual
-
-
-				//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-				//Para colocar el titulo con el trimestre adecuado
-
-				if($mesActual == 'Septiembre' || $mesActual == 'Octubre' || $mesActual == 'Noviembre' || $mesActual == 'Diciembre'){
-					$mes1 = 'Septiembre';
-					$mes2 = 'Diciembre';
-				}else{
-
-					switch ($arrayFecha[2] % 3) {
-						case 1:
-							$mes1 = $mesActual;
-							$mes2 = $Meses[$arrayFecha[2] + 2];
-							break;
-						
-						case 2:
-							$mes1 = $Meses[$arrayFecha[2] - 1];
-							$mes2 = $Meses[$arrayFecha[2] + 1];
-							break;
-
-						default:
-							$mes1 = $Meses[$arrayFecha[2] - 2];
-							$mes2 = $mesActual;					
-							break;
-					}
-					
-				}
-
-
-			 ?>
+			<?php require '../Scripts/conexionDB.php'; ?>
 			
 			<div class="titulo-calendario">
 				<?php echo strtoupper($mes1).' - '.strtoupper($mes2).' '.$arrayFecha[0]; ?>
